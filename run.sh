c3e987a458a49a3a3bb7d84631833d7dd2cea74c
peopleig <patelprachet@gmail.com>
2026-08-16 20:27:09 +0530

    Update run.sh - Provide test pass/fail statistics

--- run.sh
+++ run.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/env bash
 EXE="${1:-./lexer}"
+pass=0
+fail=0
+GREEN="\033[1;32m"
+RED="\033[1;31m"
+CYAN="\033[1;36m"
+ENDCLR="\033[0m"
 
 for f in test/*.min; do
     echo "Running: $f"
     "$EXE" "$f"
+    EXIT_CODE=$?
+    if [[ "$f" == *"error"* ]];then
+        if [ "$EXIT_CODE" -eq 2 ]; then
+            echo -e "Result: ${GREEN}PASS${ENDCLR}"
+            ((pass++))
+        else
+            echo -e "Result: ${RED}FAIL${ENDCLR} (Expected lexical errors)"
+            ((fail++))
+        fi
+    else
+        if [ "$EXIT_CODE" -eq 0 ]; then
+            echo -e "Result: ${GREEN}PASS${ENDCLR}"
+            ((pass++))
+        else
+            echo -e "Result: ${RED}FAIL${ENDCLR}"
+            ((fail++))
+        fi
+    fi
     echo
 done
+
+echo -e "${CYAN}TEST RESULTS${ENDCLR}"
+echo -e "${GREEN}Pass${ENDCLR}: ${pass}"
+echo -e "${RED}Fail${ENDCLR}: ${fail}"
